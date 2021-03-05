985a00c95824758ef9c29d662416c2258264dcd4
Arm Sukrit <arm.xdsk@hotmail.com>
2021-03-05 23:48:01 +0700

    first stable version
    add handler to trying to download an unavailable video such as premium only vid

--- ytget.py
+++ ytget.py
@@ -5,7 +5,7 @@ import sys
 for i in range(3):
     try:
         from pytube import YouTube, Playlist
-        from pytube.exceptions import RegexMatchError
+        from pytube.exceptions import RegexMatchError, VideoUnavailable, VideoRegionBlocked, VideoPrivate
     except ImportError:
         if i == 1:
             print("Cannot install dependencies...")
@@ -43,7 +43,7 @@ def main():
         os.chdir(os.path.join(file_dir, downloaded_videos_folder))
 
     try:
-        vid = YouTube(link)
+        vid = filtered_video(link)
     except RegexMatchError:
         try:
             playlist = Playlist(link)
@@ -53,22 +53,42 @@ def main():
             sleep(3)
             exit(1)
         else:
-            videos = playlist.videos
+            print("Trying to download " + str(len(playlist)) + " video(s).")
             with ThreadPoolExecutor() as ex:
-                ex.map(download_video, videos, [
-                       only_audio for _ in range(len(playlist))])
+                ex.map(download_video_from_url, playlist.video_urls, [
+                    only_audio for _ in range(len(playlist))])
     else:
+        if not vid:  # cannot download this vid
+            exit(1)
         download_video(vid, only_audio)
 
 
-def download_video(vid: YouTube, only_audio=None, output_path=None):
-    print(f"Downloading {vid.title} ...")
+def filtered_video(url: str):
+    # check if this video of this url can be downloaded or not
+    # return YouTube(url) if available for download else None
+
+    try:
+        return YouTube(url)
+    except (VideoPrivate, VideoRegionBlocked, VideoUnavailable):
+        print(
+            f'Video id, {url.split("youtu.be/")[1] if "youtu.be/" in url else url.split("v=")[1].split("&")[0]}, is not available to download.')
+        return None
+
+
+def download_video_from_url(url: str, only_audio=None):
+    vid = filtered_video(url)
+    if not vid:
+        return
+    download_video(vid, only_audio)
+
+
+def download_video(vid: YouTube, only_audio=None):
     if only_audio:
         best_stream = vid.streams.get_audio_only()
         best_stream.download()
     else:
-        vid.streams.get_highest_resolution().download()
-    print(f"Downloaded {vid.title}!")
+        vid.streams.first().download()
+    print(f"\tDownloaded {vid.title}")
 
 
 if __name__ == "__main__":
