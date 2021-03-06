0e2e3d8509b3f63a0516bead310a9ccdc57a6a7f
Arm Sukrit <arm.xdsk@hotmail.com>
2021-03-06 12:00:38 +0700

    updated

--- add_to_path.py
+++ add_to_path.py
@@ -1,8 +1,17 @@
 import os
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_dir)
 
-cmd = f"setx path {file_dir}"
+print(os.system("path > path.txt"))
+with open("path.txt") as f:
+    paths = f.read().replace("PATH=", "").replace("\n", "").split(';')
 
-if os.system(cmd) == 0:
-    print("Added " + file_dir + " to path.")
+if file_dir in paths:
+    print(f"{file_dir} is already in PATH.")
+else:
+    cmd = f"setx path \"%path%;{file_dir}\""
+    if os.system(cmd) == 0:
+        print("Added " + file_dir + " to PATH.")
+    else:
+        print("Cannot add " + file_dir + " to PATH.")
